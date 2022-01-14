d723699c6f5849f58d9dc2ac661f7e1d69737176
Nevilkumar <nevil.0619@gmail.com>
2022-01-14 12:51:55 +0530

    Backend Ready

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema.js');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/graphql', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connection Successfull"))
+    .catch((err) => console.log(err));
+
+
+
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+app.listen(8000, () => {
+    console.log("Listening on port 8000");
+})
